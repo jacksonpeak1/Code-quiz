cd33d5007a57623075227cc8502c48b79774aea7
Jackson Peak <jacksonpeak1@icloud.com>
2021-03-21 12:55:55 -0400

    Added classlist and navigate from welcome screen to first question

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 var startBtn = document.getElementById("start-btn")
+var welcomeScreen = document.getElementById('welcome-screen')
+var questionScreen = document.getElementById('question-screen')
 
 startBtn.addEventListener("click", startGame)
 
 function startGame(){
     console.log("start game function triggered")
+    welcomeScreen.classList.add("hidden")
+    questionScreen.classList.remove("hidden")
 }
